b0af37389c6da830757316fc503671d389d8d371
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-03-09 15:28:47 -0500

    hook skeleton code in place

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useDarkMode } from "./hooks/useDarkMode";
 import axios from "axios";
 
 import Charts from "./components/Charts";
@@ -15,8 +16,8 @@ const App = () => {
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export function useDarkMode() {}
